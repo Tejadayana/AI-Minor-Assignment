75f355ecd05c7cc8961bb0d308d7f7cc92e163fd
Tejadayana <99782556+Tejadayana@users.noreply.github.com>
2024-09-25 10:56:27 +0530

    Update Q2.pl

--- Q2.pl
+++ Q2.pl
@@ -1,50 +1,38 @@
 #!/usr/bin/swipl -f -q
 
-/* Function for printing list */
-writenlist([]) :- nl.
-writenlist([H|T]) :-
-    write(H),
-    write(' '),
-    writenlist(T).
-
-/* River bank change rules */
+% River bank change rules
 opposite(e, w).
 opposite(w, e).
 
-/* Check if a state is unsafe (i.e., more than 100 kg in the boat) */
-unsafe(state(_, BoatPassengers)) :-
-    sum_list(BoatPassengers, TotalWeight),
+% Check if the boat is overloaded
+unsafe(Passengers) :-
+    sum_list(Passengers, TotalWeight),
     TotalWeight > 100.
 
-/* Move function for valid river crossings */
+% Move function for valid river crossings
 move(state(X, Bank1, Bank2), state(Y, NewBank1, NewBank2)) :-
     opposite(X, Y),
-    transfer(Bank1, NewBank1, Bank2, NewBank2),
-    not(unsafe(state(Y, NewBank1))),
-    writenlist(['Try: Move from ', X, ' to ', Y, ' with ', Bank2]).
+    select(Person, Bank1, NewBank1),
+    NewBank2 = [Person|Bank2],
+    \+ unsafe(NewBank2),  % Check if the new state is safe
+    write('Move from '), write(X), write(' to '), write(Y), write(' with '), writenlist([Person]), nl.
 
-/* Define valid transfers between river banks */
-transfer([A|Bank1], Bank1, Bank2, [A|Bank2]) :-
-    sum_list(Bank2, Sum),
-    Sum + A =< 100.
+% Print list elements
+writenlist([]) :- nl.
+writenlist([H|T]) :- write(H), write(' '), writenlist(T).
 
-transfer([A,B|Bank1], Bank1, Bank2, [A,B|Bank2]) :-
-    sum_list(Bank2, Sum),
-    Sum + A + B =< 100.
-
-/* Solution path */
+% Solution path
 path(Goal, Goal, List) :-
     write('Solution Path: '), nl,
     reverse_writenlist(List).
 
-/* Make moves until the goal state is reached */
+% Make moves until the goal state is reached
 path(State, Goal, List) :-
     move(State, NextState),
-    not(member(NextState, List)),
-    path(NextState, Goal, [NextState|List]),
-    !.
+    \+ member(NextState, List),
+    path(NextState, Goal, [NextState|List]).
 
-/* Initial call to run the program */
+% Initial call to run the program
 :- 
-    path(state(e, [80,80,30,30], []), state(w, [], [80,80,30,30]), [state(e, [80,80,30,30], [])]),
+    path(state(e, [80, 80, 30, 30], []), state(w, [], [80, 80, 30, 30]), [state(e, [80, 80, 30, 30], [])]),
     halt(0).
